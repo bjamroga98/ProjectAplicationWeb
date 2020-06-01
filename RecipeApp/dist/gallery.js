a6be367d4b4adbf33babf8e43490bcd901d09bb6
Queen98 <basia.jamroga@gmail.com>
2020-06-01 08:49:30 +0200

    mini change

--- RecipeApp/dist/gallery.js
+++ RecipeApp/dist/gallery.js
@@ -1,13 +1,13 @@
 function imageGallery() {
-    const highlight = document.querySelector(".gallery-hightlight");
+    const galleries = document.querySelector(".galleries");
     const previews = document.querySelectorAll(".preview img");
   
     previews.forEach(preview => {
       preview.addEventListener("click", function() {
-        const smallSrc = this.src;
-        const bigSrc = smallSrc.replace("small", "big");
+        const smallphoto = this.src;
+        const bigphoto = smallphoto.replace("small", "big");
         previews.forEach(preview => preview.classList.remove("recipe-active"));
-        highlight.src = bigSrc;
+        galleries.src = bigphoto;
         preview.classList.add("recipe-active");
       });
     });
